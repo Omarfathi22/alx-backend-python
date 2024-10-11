7c4764a3aed399886561f7c26dda658bbdddc55d
Omarfathi22 <ommarf525@gmail.com>
2024-10-11 17:34:01 +0300

    Pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,6 +3,7 @@
 Module that provides a function to add two floats.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Return the sum of two floats.
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,7 @@
 Module that provides a function to concatenate two strings.
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Return the concatenation of two strings.
