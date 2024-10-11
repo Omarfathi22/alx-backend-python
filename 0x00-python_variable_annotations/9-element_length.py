bd68d84ba7a13dffb564a641e5eb7b745fe579c9
Omarfathi22 <ommarf525@gmail.com>
2024-10-11 17:48:18 +0300

    9-fix

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 """
-Module that provides a function to compute the length of each element in an iterable.
+Module that provides a function to compute the length of each
+element in an iterable.
 """
 
 from typing import Iterable, Sequence, List, Tuple
 
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Return a list of tuples with each element and its length.
+    
+    Return a list of tuples with each 
+    element and its length.
 
     Args:
-        lst (Iterable[Sequence]): An iterable of sequences (like lists or strings).
+        
+        lst (Iterable[Sequence]): An iterable of 
+        sequences (like lists or strings).
 
     Returns:
         List[Tuple[Sequence, int]]: A list of tuples, each containing an 
